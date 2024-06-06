ece826e058312272f27bdca8aafff5b0a0651c87
BEE-gfc <17681755365@163.com>
2024-06-07 02:05:26 +0800

    First commit

--- 24bit to 16bit-1.1.0.py
+++ 24bit to 16bit-1.1.0.py
@@ -2,8 +2,8 @@
 此程序用于将24位颜色模式的图片转换为16位的图片(也就是RGB565模式),并将其读取出的像素数据写入头文件中。
 主要用于开发TFT屏,在上面显示任意经过处理的图片
 
-此项目创建于2024年6月4日23:01
-版本号为1.0.0
+此项目创建于2024年6月7日23:01
+版本号为1.1.0
 """
 
 from PIL import Image
@@ -45,44 +45,3 @@ def read_image(im):
         f.write(text)
 
 read_image(pic)
-
-#以下是通过AI修饰的代码
-
-'''
-from PIL import Image
-
-file_path = "C:/Users/86176/Desktop/image.h"
-pic = 'C:/Users/86176/Desktop/xt.jpg'
-
-def read_image(pic):
-    img = Image.open(pic)
-    old_w, old_h = img.size
-    dim = (160, 128)   # 目标尺寸
-    img.thumbnail(dim, Image.ANTIALIAS)
-
-    text =''
-    new_w, new_h = img.size
-
-    text += "#define IMAGE_WIDTH  %d\n" % new_w
-    text += "#define IMAGE_HEIGHT %d\n" % new_h
-    text += "uint16_t static const PROGMEM image[] = {\n"
-
-    for y in range(0, new_h):
-        for x in range(0, new_w):
-            rgb565 = ((img.getpixel((x, y))[0] & 0xf8) << 8) + ((img.getpixel((x, y))[1] & 0xfc) << 3) + (img.getpixel((x, y))[2] >> 3)
-            new_rgb565 = str(hex(rgb565))
-
-            if (x == new_w - 1 and y == new_h - 1):
-                text += "%s" % new_rgb565
-            elif (x == new_w - 1):
-                text += "%s,\n" % new_rgb565
-            else:
-                text += "%s, " % new_rgb565
-
-    text += "\n};"
-
-    with open(file_path, "w") as f:
-        f.write(text)
-
-read_image(pic)
-'''

c5d6c03d2c9e22db08ebb6b77a9cf51ac5e4ebf7
BEE-gfc <17681755365@163.com>
2025-03-30 16:59:58 +0800

    gradup

--- IP_L-0.3.0.py
+++ IP_L-0.3.0.py
@@ -6,11 +6,14 @@
 时间: 2024-06-11-03-36
 '''
 
+import os
+import sys
 import tkinter as tk
 from tkinter import ttk
 from pic_GUI import PicGUI
 from gif_GUI import Gif_P
-from Readme import readme
+from flac_add import Add_Flac_Cover
+from Readme import readme,MousePosition
 
 def show_imgarray():
     tab_control.select(0)  # 选中图像数组选项卡
@@ -18,11 +21,14 @@ def show_imgarray():
 def show_gif_extraction():
     tab_control.select(1)  # 选中GIF提取选项卡
 
+def show_Add_Flac_Cover():
+    tab_control.select(2)  # 选中添加封面选项卡
+
 def show_Readme():
-    tab_control.select(2)
+    tab_control.select(3)
 
 root = tk.Tk()
-root.iconbitmap(r'C:\Users\86176\Desktop\Project Library\VSC-E\Wei-RC-Lab\Python-lab\image\t.ico')
+# root.iconbitmap(r'C:\Users\86176\Desktop\Project Library\VSC-E\Wei-RC-Lab\Python-lab\image\t.ico')
 root.title("集成处理实验室")
 root.geometry("400x400")
 root.minsize(400, 400)
@@ -31,17 +37,23 @@ root.resizable(False, False)
 # 选项卡控件
 tab_control = ttk.Notebook(root)
 
-imgarray_frame = ttk.Frame(tab_control)
-gif_extraction_frame = ttk.Frame(tab_control)
+imgarray_tab = ttk.Frame(tab_control)
+Add_Flac_Cover_frame = ttk.Frame(tab_control)
+gif_tab = ttk.Frame(tab_control)
+mouse_position_frame = ttk.Frame(tab_control)
 Readme_frame = ttk.Frame(tab_control)
 
-tab_control.add(imgarray_frame, text="▼")
-tab_control.add(gif_extraction_frame, text="▲")
-tab_control.add(Readme_frame, text="README")
+tab_control.add(imgarray_tab, text="RGB888图像数组")
+tab_control.add(gif_tab, text="GIF")
+tab_control.add(Add_Flac_Cover_frame, text="FLAC封面")  # 新增选项卡
+tab_control.add(mouse_position_frame, text="坐标")
+tab_control.add(Readme_frame, text="帮助文档")
 tab_control.pack(expand=1, fill="both")
 #实例化类
-PicGUI(imgarray_frame)
-Gif_P(gif_extraction_frame)
+PicGUI(imgarray_tab)
+Gif_P(gif_tab)
+Add_Flac_Cover(Add_Flac_Cover_frame)
+MousePosition(mouse_position_frame)
 readme(Readme_frame)
 
 root.mainloop()

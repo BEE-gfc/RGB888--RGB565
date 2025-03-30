4a7696d5db10a14ba662f08c29238b20e983781e
BEE-gfc <17681755365@163.com>
2025-03-30 16:55:26 +0800

    gradup

--- Readme.py
+++ Readme.py
@@ -3,6 +3,7 @@
 '''
 
 import tkinter as tk
+import pyautogui
 
 class readme:
     def show_add_info(self):
@@ -12,7 +13,12 @@ class readme:
         add_info_text.pack(expand=True, fill='both')
 
         add_info_content = """
-这里是详细的使用说明:
+2024-06-11
+V.0.1.0
+        
+2025/3/29
+V.0.4.0
+新增flac音频文件封装封面功能
 """
         add_info_text.insert("1.0", add_info_content)
         add_info_text.config(state='disabled')
@@ -33,7 +39,7 @@ class readme:
         简单说明:
 
         本软件是用纯Python编写的图像处理软件
-        本来想用C++来编写,但是最近在学习Python
+        本想用C++来编写,但是最近在学习Python
         于是就当作练手项目来做了
 
         这个版本我新增了逐帧提取GIF的功能
@@ -57,5 +63,32 @@ class readme:
         self.readme_text.config(state='disabled')
         
         # 创建按钮
-        add_info_button = tk.Button(self.root, text="使用说明", command=self.show_add_info)
+        add_info_button = tk.Button(self.root, text="开发日志", command=self.show_add_info)
         add_info_button.pack(side="bottom", anchor="se")  # 将按钮显示在右下角，并且稍微突出显示
+
+class MousePosition:
+    def __init__(self, parent_frame):
+        self.parent_frame = parent_frame
+        self.create_widgets()
+        self.update_mouse_position()
+
+    def create_widgets(self):
+        self.position_label = tk.Label(self.parent_frame, text="鼠标坐标: (0, 0)", font=("Arial", 12))
+        self.position_label.pack(pady=20)
+
+    def update_mouse_position(self):
+        x, y = pyautogui.position()
+        self.position_label.config(text=f"x:{x}, y:{y}")
+        self.parent_frame.after(50, self.update_mouse_position)  # 每50毫秒更新一次
+
+'''
+import pyautogui
+
+try:
+    while True:
+        # 获取鼠标在屏幕上的坐标
+        x, y = pyautogui.position()
+        print(f"鼠标坐标: ({x}, {y})", end='\r')
+except KeyboardInterrupt:
+    print("\n程序已结束")
+'''
